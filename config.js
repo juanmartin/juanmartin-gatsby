d15f4ededfb2960f4afa6066b073c81b5b8eba90
Juan Martin <juansm91@gmail.com>
2020-04-15 03:36:58 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteTitle: 'Juan Martin', // <title>
+  manifestName: 'juanmartin',
+  manifestShortName: 'juanmartin', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Juan Martin Sesali Maydana',
+  subHeading: 'Full time freelancer. Hago música y resuelvo problemas.',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/juanmartin',
+    },
+    // {
+    //   icon: 'fa-twitter',
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/jmartinsm',
+    // },
+    // {
+    //   icon: 'fa-facebook',
+    //   name: 'Facebook',
+    //   url: 'https://facebook.com/theanubhav',
+    // },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:juanmartinsesali@gmail.com',
+    },
+  ],
+  // phone: '000-00000',
+  // address: '305 Del Sol, Sunnyvale, CA',
+};
